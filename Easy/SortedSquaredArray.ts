1a58308c9871b658712184362fa924c8c5617a8e
Adeola Samuel <meetprecious1403@gmail.com>
2025-02-19 23:26:21 +0100

    optimized method

--- Easy/SortedSquaredArray.ts
+++ Easy/SortedSquaredArray.ts
@@ -5,3 +5,22 @@ function sortedSquarredArray(array: number[]): number[] {
   }
   return sortedSquarredArray.sort((a, b) => a - b);
 }
+
+//assuming the array is sorted
+function sortedSquarredArrayOptimized(array: number[]): number[] {
+  const newArray: number[] = new Array(array.length);
+  let leftPointer = 0;
+  let rightPointer = array.length - 1;
+  for (let i = array.length - 1; i >= 0; i--) {
+    const leftSquared = Math.pow(array[leftPointer], 2);
+    const rightSquared = Math.pow(array[rightPointer], 2);
+    if (leftSquared > rightSquared) {
+      newArray[i] = leftSquared;
+      leftPointer++;
+    } else {
+      newArray[i] = rightSquared;
+      rightPointer--;
+    }
+  }
+  return newArray;
+}
