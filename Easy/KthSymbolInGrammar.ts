80cc32085d951586e81a8dc9a4a1f8d594b28310
Adeola Samuel <meetprecious1403@gmail.com>
2025-02-22 23:43:53 +0100

    Josephus

--- Easy/KthSymbolInGrammar.ts
+++ Easy/KthSymbolInGrammar.ts
@@ -10,3 +10,12 @@ function kthGrammmar(n: number, m: number): number {
     return 1 - kthGrammmar(n - 1, m - mid);
   }
 }
+
+//Josephus Problem
+const josephus = (n: number, k: number): number => {
+  let res = 0;
+  for (let i = 2; i <= n; i++) {
+    res = (res + k) % i;
+  }
+  return res + 1;
+};
