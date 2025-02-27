4a917ac503d452c190fd156e0d76df53843745ed
Adeola Samuel <meetprecious1403@gmail.com>
2025-02-27 23:35:05 +0100

    different approach

--- Medium/Josephus.ts
+++ Medium/Josephus.ts
@@ -1,4 +1,19 @@
-//Josephus Problem
+var findTheWinner = function(n, k) {
+  let arr = Array.from({length:n},(_,i) => i+1)
+  
+ function helper (n, startIndex){
+  if (n.length === 1){
+      return n[0]
+  }
+  let loserIndex = (startIndex + k-1)% arr.length
+  arr.splice(loserIndex, 1)
+  return helper(arr, loserIndex)
+  
+ }
+ return helper(arr,0)
+};
+
+//Josephus Problem Optimized
 const josephus = (n: number, k: number): number => {
   let res = 0;
   for (let i = 2; i <= n; i++) {
