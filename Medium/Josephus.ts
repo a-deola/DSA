dfdeb52c3bad4903a68f80d23e5f680c00159549
Adeola Samuel <meetprecious1403@gmail.com>
2025-02-28 09:39:35 +0100

    update types

--- Medium/Josephus.ts
+++ Medium/Josephus.ts
@@ -1,16 +1,15 @@
-var findTheWinner = function(n, k) {
-  let arr = Array.from({length:n},(_,i) => i+1)
-  
- function helper (n, startIndex){
-  if (n.length === 1){
-      return n[0]
+var findTheWinner = function (n: number, k: number): number {
+  let arr: number[] = Array.from({ length: n }, (_, i) => i + 1);
+
+  function helper(n: number[], startIndex: number) {
+    if (n.length === 1) {
+      return n[0];
+    }
+    let loserIndex = (startIndex + k - 1) % arr.length;
+    arr.splice(loserIndex, 1);
+    return helper(arr, loserIndex);
   }
-  let loserIndex = (startIndex + k-1)% arr.length
-  arr.splice(loserIndex, 1)
-  return helper(arr, loserIndex)
-  
- }
- return helper(arr,0)
+  return helper(arr, 0);
 };
 
 //Josephus Problem Optimized
