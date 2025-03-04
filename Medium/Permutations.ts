8db8dd593a53fedabeae7c173d423a964c5d9d4a
Adeola Samuel <meetprecious1403@gmail.com>
2025-03-04 23:21:18 +0100

    unique permutation

--- Medium/Permutations.ts
+++ Medium/Permutations.ts
@@ -18,3 +18,29 @@ const permute = (nums: number[]): number[][] => {
   helper(0);
   return result;
 };
+
+const permuteUnique = (nums: number[]): number[][] => {
+  let result: number[][] = [];
+  let n = nums.length;
+
+  function helper(i: number) {
+    if (i === n - 1) {
+      result.push([...nums]);
+      return;
+    }
+    let hash = {};
+
+    for (let j = i; j < n; j++) {
+      if (!hash[nums[j]]) {
+        hash[nums[j]] = 1;
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+        helper(i + 1);
+        //backtrack
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      }
+    }
+  }
+
+  helper(0);
+  return result;
+};
