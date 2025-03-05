2dd5e998f5f8a40a7a8185da6809683bbdcf4a50
Adeola Samuel <meetprecious1403@gmail.com>
2025-03-05 23:39:25 +0100

    power sets recursive

--- Medium/PowerSet.ts
+++ Medium/PowerSet.ts
@@ -10,4 +10,24 @@ const powerSet = (arr: number[]): number[][] => {
   return result;
 };
 
-console.log(powerSet([1, 2, 3]));
+//recursive solution
+const subsets = function (nums: number[]): number[][] {
+  //Write code here
+  //[1,3,5]
+  let result: number[][] = [];
+  function helper(nums: number[], i: number, subset: number[]) {
+    if (i === nums.length) {
+      result.push(subset.slice());
+      return;
+    }
+
+    helper(nums, i + 1, subset);
+    subset.push(nums[i]);
+    helper(nums, i + 1, subset);
+    subset.pop();
+  }
+  helper(nums, 0, []);
+  return result;
+};
+
+//console.log(powerSet([1, 2, 3]));
