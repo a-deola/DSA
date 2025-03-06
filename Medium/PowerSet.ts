05080e66f3fee267abb5129864ddbc3e9a55054c
Adeola Samuel <meetprecious1403@gmail.com>
2025-03-06 23:22:54 +0100

    sets with duplicates

--- Medium/PowerSet.ts
+++ Medium/PowerSet.ts
@@ -12,8 +12,6 @@ const powerSet = (arr: number[]): number[][] => {
 
 //recursive solution
 const subsets = function (nums: number[]): number[][] {
-  //Write code here
-  //[1,3,5]
   let result: number[][] = [];
   function helper(nums: number[], i: number, subset: number[]) {
     if (i === nums.length) {
@@ -31,3 +29,23 @@ const subsets = function (nums: number[]): number[][] {
 };
 
 //console.log(powerSet([1, 2, 3]));
+
+function subsetsWithDup(nums: number[]): number[][] {
+  let result: number[][] = [];
+  nums.sort((a, b) => a - b);
+  function helper(index: number, subset: number[]) {
+    if (index === nums.length) {
+      result.push([...subset]);
+      return;
+    }
+    subset.push(nums[index]);
+    helper(index + 1, subset);
+    subset.pop();
+    while (index < nums.length - 1 && nums[index] === nums[index + 1]) {
+      index++;
+    }
+    helper(index + 1, subset);
+  }
+  helper(0, []);
+  return result;
+}
