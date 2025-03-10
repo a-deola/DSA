3d41a1e0f14237d90bc5025ab054ebc8dca6097a
Adeola Samuel <meetprecious1403@gmail.com>
2025-03-10 23:58:31 +0100

    optimized

--- Easy/RansomNote.ts
+++ Easy/RansomNote.ts
@@ -14,3 +14,27 @@ function canConstruct(ransomNote: string, magazine: string): boolean {
   return true;
 }
 // TC: O(n*m), SC: O(n)
+
+function canConstructOptimized(ransomNote: string, magazine: string): boolean {
+  const magazineMap = new Map<string, number>();
+  for (let i = 0; i < magazine.length; i++) {
+    let letter = magazine.charAt(i);
+    let currentCount = magazineMap.get(letter) || 0;
+
+    magazineMap.set(letter, currentCount + 1);
+  }
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    let letter = ransomNote.charAt(i);
+    let currentCount = magazineMap.get(letter) || 0;
+
+    if (currentCount === 0) {
+      return false;
+    }
+
+    magazineMap.set(letter, currentCount - 1);
+  }
+  return true;
+}
+
+// TC: O(m), SC: O(1)
